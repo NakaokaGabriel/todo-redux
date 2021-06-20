559b9750e83790ad69ad549476e4b92e334edddf
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-06-20 01:22:55 -0300

    add todo list structure and create header style container

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { 
-  FaEllipsisH, 
-  FaRocketchat, 
-  FaShareSquare, 
-  FaSortAmountDownAlt 
+import {
+  FaEllipsisH,
+  FaRocketchat,
+  FaShareSquare,
+  FaSortAmountDownAlt,
+  FaAngleDown,
+  FaCodeBranch
 } from 'react-icons/fa';
 
 import * as Style from './styles';
@@ -12,7 +14,7 @@ const Todo = () => {
   return (
     <Style.Container>
       <Style.Header>
-        <h1>Title</h1>
+        <h1>Martonis</h1>
 
         <Style.HeaderActions>
           <button type="button">
@@ -33,6 +35,30 @@ const Todo = () => {
           </button>
         </Style.HeaderActions>
       </Style.Header>
+
+      <Style.TodoList>
+        <button type="button">
+          <FaAngleDown />
+        </button>
+
+        <Style.ItemChecked htmlFor="item-1">
+          <input type="checkbox" name="item-1" id="item-1" />
+        </Style.ItemChecked>
+        
+        <Style.ItemContent>
+          <p>Alert Page</p>
+          <Style.ContentAction>
+            <button type="button">
+              <FaCodeBranch />
+              0/2
+            </button>
+            <button type="button">
+              <FaRocketchat />
+              4
+            </button>
+          </Style.ContentAction>
+        </Style.ItemContent>
+      </Style.TodoList>
     </Style.Container>
   );
 }
--- src/components/Todo/styles.ts
+++ src/components/Todo/styles.ts
@@ -1,7 +1,26 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 800px;
+  margin: 4rem auto 0;
+`;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 1.4rem;
+  }
+`;
 
 export const HeaderActions = styled.div``;
+
+export const TodoList = styled.div``;
+
+export const ItemChecked = styled.label``;
+
+export const ItemContent = styled.div``;
+
+export const ContentAction = styled.div``;
